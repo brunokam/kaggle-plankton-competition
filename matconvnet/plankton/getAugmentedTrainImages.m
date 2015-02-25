8a67e0cd6d4b9c2cde743cddd57dd31473b256f1
Mateusz Bruno-Kaminski <m.bruno.kaminski@gmail.com>
2015-02-25 14:26:28 +0100

    Augmentation fix
    I've fixed some errors connected with augmented image labels.

--- matconvnet/plankton/getAugmentedTrainImages.m
+++ matconvnet/plankton/getAugmentedTrainImages.m
@@ -14,30 +14,52 @@ fprintf('Calculating number of rotations... ');
 rotatedImagesNum = 0;
 numByClasses = zeros(weakClassesNum, 1);
 for i = 1 : weakClassesNum
-    ids = find(labels == weakClasses(i));
-    [num, ~] = size(ids);
+    classImageIds = find(labels == weakClasses(i));
+    [num, ~] = size(classImageIds);
 
     numByClasses(i) = num;
     rotatedImagesNum = rotatedImagesNum + (num * rotationsPerImgNum);
+end
+
+ids = zeros(sum(numByClasses), 2);
+k = 1;
+for i = 1 : weakClassesNum
+    classId = weakClasses(i);
+    classImageIds = find(labels == classId);
+
+    for j = 1 : numByClasses(i)
+        ids(k, :) = [classImageIds(j), classId];
+        k = k + 1;
+    end
 end
 fprintf('Done.\n');
 
 % Rotates images
 fprintf('Rotating images... ');
 rotatedImages = zeros(rotatedImagesNum, imgSize * imgSize);
+rotatedImageClassIds = zeros(rotatedImagesNum, 1);
 randomIds = zeros(rotatedImagesNum, 1);
-id = 1;
+k = 1;
+m = 1;
 for i = 1 : weakClassesNum
     for j = 1 : numByClasses(i)
-        img = images(i, :);
+        img = images(ids(k, 1), :);
 
-        for k = 1 : rotationsPerImgNum % Do specified numer of rotations
-            rotatedImg = imrotate(img, k * (360 / (rotationsPerImgNum + 1)));
+        for l = 1 : rotationsPerImgNum % Do specified numer of rotations
+            rotatedImg = imrotate(img, l * (360 / (rotationsPerImgNum + 1)));
 
-            randomIds(id) = randi(imagesNum + rotatedImagesNum - 1);
-            rotatedImages(id, :) = rotatedImg(:);
-            id = id + 1;
+            rnd = randi(imagesNum + rotatedImagesNum - 1);
+            while any(randomIds == rnd) == 1
+                rnd = randi(imagesNum + rotatedImagesNum - 1);
+            end
+
+            randomIds(m) = rnd;
+            rotatedImages(m, :) = rotatedImg(:);
+            rotatedImageClassIds(m) = ids(k, 2);
+            m = m + 1;
         end
+
+        k = k + 1;
     end
 
     % fprintf('%i / %i classes\n', i, weakClassesNum);
@@ -47,19 +69,26 @@ fprintf('Done.\n');
 % Merges images
 fprintf('Merging images... ');
 finalImages = zeros(imagesNum + rotatedImagesNum, imgSize * imgSize);
+finalLabels = zeros(imagesNum + rotatedImagesNum, 1);
 for i = 1 : rotatedImagesNum
-    finalImages(randomIds(i), :) = rotatedImages(i);
+    finalImages(randomIds(i), :) = rotatedImages(i, :);
+    finalLabels(randomIds(i)) = rotatedImageClassIds(i);
 end
 
 j = 1;
-for i = 1 : imagesNum
-    if any(finalImages(i, :)) == 0 % Writes original images between rotated ones
+for i = 1 : (imagesNum + rotatedImagesNum)
+    if any(randomIds == i) == 0 % Writes original images between rotated ones
         finalImages(i, :) = images(j, :);
+        finalLabels(i) = labels(j);
         j = j + 1;
     end
 end
 fprintf('Done.\n');
 
 % Saves images
+fprintf('Saving images... ');
 augmentedTrainImages28 = finalImages;
+augmentedLabels = finalLabels;
 save(strcat('../../imageData/augmentedTrainImages', int2str(imgSize)), 'augmentedTrainImages28', '-v7.3');
+save('augmentedLabels', 'augmentedLabels', '-v7.3');
+fprintf('Done.\n');
--- matconvnet/plankton/getTestImages.m
+++ matconvnet/plankton/getTestImages.m
@@ -2,13 +2,13 @@ load('testNames.mat')  % Load file names
 parpool(4);  % Set workers for paraller computations
 
 % Set parameters
-size = 40;  % Size of images desired
+imgSize = 28;  % Size of images desired
 imagesNumber = 60000;  % Total number of images to read
 
 % Read images
-images = zeros(imagesNumber, size * size);  % Initialize images matrix
+images = zeros(imagesNumber, imgSize * imgSize);  % Initialize images matrix
 parfor i = 1 : imagesNumber
-    img = imresize(imread(testNames{i,1}), [size size]);
+    img = imresize(imread(testNames{i,1}), [imgSize imgSize]);
     images(i,:) = img(:);
     if mod(i, 100) == 0
         fprintf('%i / %i \n', i, imagesNumber);
@@ -16,9 +16,9 @@ parfor i = 1 : imagesNumber
 end
 
 % Save images
-testImages40one = images;
+testImages28one = images;
 mkdir('../../imageData')
-save(strcat('../../imageData/testImages', int2str(size), 'one'), 'testImages40one', '-v7.3');
+save(strcat('../../imageData/testImages', int2str(imgSize), 'one'), 'testImages28one', '-v7.3');
 clear images;
 
 
@@ -26,9 +26,9 @@ clear images;
 imagesNumberTwo = 70400;  % Total number of images to read
 
 % Read second set
-images = zeros(imagesNumberTwo, size * size);  % Initialize images matrix
+images = zeros(imagesNumberTwo, imgSize * imgSize);  % Initialize images matrix
 parfor i = (imagesNumber + 1) : imagesNumberTwo
-    img = imresize(imread(testNames{i,1}), [size size]);
+    img = imresize(imread(testNames{i,1}), [imgSize imgSize]);
     images(i - imagesNumber,:) = img(:);
     if mod(i, 100) == 0
         fprintf('%i / %i \n', i, imagesNumberTwo);
@@ -36,7 +36,8 @@ parfor i = (imagesNumber + 1) : imagesNumberTwo
 end
 
 % Save second set
-testImages40two = images;
+testImages28two = images;
 mkdir('../../imageData')
-save(strcat('../../imageData/testImages', int2str(size), 'two'), 'testImages40two', '-v7.3');
+save(strcat('../../imageData/testImages', int2str(imgSize), 'two'), 'testImages28two', '-v7.3');
 clear images;
+delete(gcp);
